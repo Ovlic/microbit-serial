6a8875dc65428d28067f03bf9c13e6507b9bc0b4
Ovlic <74332950+Ovlic@users.noreply.github.com>
2021-12-13 14:47:37 -0800

    Initial Commit

---
+++ recieve.py
@@ -0,0 +1,116 @@
+
+# "serial" is a module that allows us to communicate with serial ports
+import serial
+
+# "subprocess" is a module that allows us to run shell commands
+import subprocess
+
+# "exist" is a function to check if a directory exists
+from os.path import exists
+
+
+# Exception registration
+class CannotOpenPort(Exception):
+    "Raised when the specified serial port doesn't exist"
+
+    def __init__(self, message="Cannot open port. Is microbit connected?"):
+        self.message = message
+    
+    def __str__(self):
+        return self.message
+
+
+class Disconnected(Exception):
+    "Raised when the microbit is disconnected"
+
+    def __init__(self, message="Microbit disconnected."):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+# Port to microbit
+port = "/dev/cu.usbmodem14102" # Change if needed; use 'ls /dev/cu.*' and look for '/dev/cu.usbmodem'
+
+# Baud rate
+baud = 115200
+
+# Check if port path exists
+if not exists(port):
+    # Cannot find port
+    raise CannotOpenPort()
+
+# Create serial connection
+s = serial.Serial(port)
+
+# Set baud rate
+s.baudrate = baud
+
+try:
+    # Loop forever or until error
+    while True:
+
+        # If the port somehow is changed, just in case
+        if not exists(port):
+            # Cannot find port
+            raise CannotOpenPort()
+
+        # Read data from serial port
+        raw_data = s.readline()
+
+        # Decode data from bytes to string
+        data = raw_data.decode('utf-8')
+
+        # Remove newline character
+        data = ' '.join(data.split())
+
+        # Print data
+        print(f"Data: '{data}'")
+        
+        # If data is 'play'
+        if data == "Play":
+            print("Resuming playback...")
+            
+            # Get contents of resume_audio.applescript
+            f = open("resume_audio.applescript", "r")
+
+            # Read contents
+            data = f.read()
+
+            # Close file
+            f.close()
+
+            # Run applescript
+            # This will run the script from "resume_audio.applescript"
+            subprocess.call(['osascript', '-e', data])
+            print("Playback resumed.")
+
+except KeyboardInterrupt:
+    # If user presses Ctrl+C
+    print("\nExiting...")
+
+    # Close serial connection
+    s.close()
+
+    print("Exited.")
+
+except OSError:
+    # On error related to the operating system (Usually if microbit is unplugged)
+    print("oserror")
+
+    # Close serial connection
+    s.close()
+
+    # Disconnected
+    raise Disconnected()
+
+except serial.serialutil.SerialException:
+    # If there is some other error related to the serial connection
+    print("serial exception")
+
+    # Close serial connection
+    s.close()
+    
+    # Raise the error (Raise means stop code with exception)
+    raise
---
+++ resume_audio.applescript
@@ -0,0 +1,14 @@
+
+-- "System Events" is the application responsible for keystrokes, mouse clicks, etc.
+
+-- Tell "System Events" to do everything between it and "end tell"
+tell application "System Events"
+    
+        -- Keystroke for "Youtube Music Desktop" app
+        keystroke space using {command down, shift down}
+
+        -- Keystroke for "Youtube" (In browser)
+        -- keystroke "k"
+
+-- End telling "System Events" to do stuff
+end tell
